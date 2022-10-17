d1c4008840563a03648ad5865c27806d4c7a13be
zzh <569941083@qq.com>
2022-10-17 19:58:17 +0800

    hot-fix test commit

--- src/main/java/com/atguigu/maven/GitTest.java
+++ src/main/java/com/atguigu/maven/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello gitHub!");
         System.out.println("hello gitHub!");
         System.out.println("hello gitHub!");
+        System.out.println("hello gitHub! HOT-fix test");
     }
 }
